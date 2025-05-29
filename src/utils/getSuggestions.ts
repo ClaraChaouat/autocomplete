e880b3ec4da9e04c1e21fb7e33dbc2984f35cc62
Clara Chaouat <clarachaouat@Claras-MacBook-Pro.local>
2025-05-29 11:06:48 +0300

    added cache

--- src/utils/getSuggestions.ts
+++ src/utils/getSuggestions.ts
@@ -1,8 +1,8 @@
 import { mockData } from '../data/mockData'
 import type { SuggestionItem } from '../types'
 
-const USE_MOCK = import.meta.env.VITE_USE_MOCK === 'true'
-const REST_COUNTRIES_API_URL = import.meta.env.VITE_REST_COUNTRIES_API || 'https://restcountries.com/v3.1/name/'
+const USE_MOCK = false
+const REST_COUNTRIES_API_URL = 'https://restcountries.com/v3.1/name/'
 
 interface CountryResponseItem {
     name: {
@@ -10,29 +10,36 @@ interface CountryResponseItem {
     }
 }
 
-/**
- * Fetches suggestions from either a public API or mock data
- * depending on the USE_MOCK flag.
- */
+const cache: Record<string, SuggestionItem[]> = {}
+
+
 export const getSuggestions = async (query: string): Promise<SuggestionItem[]> => {
-    if (!query.trim()) return []
+    const normalizedQuery = query.toLowerCase().trim()
+    if (!normalizedQuery) return []
+
+
+    if (cache[normalizedQuery]) return cache[normalizedQuery]
 
     if (USE_MOCK) {
-        const normalizedQuery = query.toLowerCase()
-        return mockData.filter(item =>
+        const filtered = mockData.filter(item =>
             item.name.toLowerCase().includes(normalizedQuery)
         )
+        cache[normalizedQuery] = filtered
+        return filtered
     }
 
     try {
-        const response = await fetch(`${REST_COUNTRIES_API_URL}${query}`)
+        const response = await fetch(`${REST_COUNTRIES_API_URL}${normalizedQuery}`)
         if (!response.ok) throw new Error(`API request failed with status ${response.status}`)
         const data: CountryResponseItem[] = await response.json()
 
-        return data.map((country, index) => ({
+        const results = data.map((country, index) => ({
             id: index,
             name: country.name.common
         }))
+
+        cache[normalizedQuery] = results
+        return results
     } catch (error) {
         console.error('API error:', error)
         throw error
