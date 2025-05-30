def41314993b33c67982ccb067464807516ce7d0
Clara Chaouat <clarachaouat@Claras-MacBook-Pro.local>
2025-05-30 19:47:46 +0300

    feat(empty the search input on click)

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -1,10 +1,7 @@
 import { useEffect } from 'react'
 import type { RefObject } from 'react'
-/**
- * Custom hook to handle clicks outside a specified element.
- * @param ref - Ref object pointing to the element to monitor for outside clicks.
- * @param handler - Function to call when an outside click is detected.
- */
+
+
 
 export function useOnClickOutside(ref: RefObject<HTMLDivElement | null>, handler: () => void) {
     useEffect(() => {
