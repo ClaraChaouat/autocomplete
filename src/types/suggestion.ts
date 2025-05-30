4261fad3235b58349f667769bcf61a1f512cea70
Clara Chaouat <clarachaouat@Claras-MacBook-Pro.local>
2025-05-30 14:43:40 +0300

    fix:file renaming

---
+++ src/types/suggestion.ts
@@ -0,0 +1,11 @@
+export interface SuggestionItem {
+    id: number;
+    name: string;
+}
+
+export interface AutocompleteProps {
+    placeholder?: string;
+    onSelect: (item: SuggestionItem) => void;
+    maxSuggestions?: number;
+    ariaLabel?: string;
+}
