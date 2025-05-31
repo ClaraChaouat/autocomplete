c00832612e502b870159f8d59c9e54b98a2ac83d
Clara Chaouat <clarachaouat@Claras-MacBook-Pro.local>
2025-05-31 17:27:10 +0300

    handled outside clicks for mobile

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -3,16 +3,20 @@ import type { RefObject } from "react";
 
 export function useOnClickOutside(
   ref: RefObject<HTMLDivElement | null>,
-  handler: () => void,
+  handler: () => void
 ) {
   useEffect(() => {
-    const handleOutsideEvent = (e: MouseEvent) => {
+    const handleOutsideEvent = (e: MouseEvent | TouchEvent) => {
       if (ref.current && !ref.current.contains(e.target as Node)) {
         handler();
       }
     };
 
     document.addEventListener("mousedown", handleOutsideEvent);
-    return () => document.removeEventListener("mousedown", handleOutsideEvent);
+    document.addEventListener("touchstart", handleOutsideEvent);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideEvent);
+      document.removeEventListener("touchstart", handleOutsideEvent);
+    };
   }, [ref, handler]);
 }
